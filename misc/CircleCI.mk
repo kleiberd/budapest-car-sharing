b83e9199b85519fe4ec01f01834d90cff2911188
Kleiber David <kleiberd93@gmail.com>
2018-08-23 18:49:20 +0200

    Build artifact

--- misc/CircleCI.mk
+++ misc/CircleCI.mk
@@ -1,5 +1,11 @@
+CIRCLECI_BUILD_FOLDER = _circleci_local_build_repo
+
 circleci-local-build:
 	$(CMD_CIRCLECI_BUILD) --volume="$(PATH_ROOT)":"/tmp/"
 
+circleci-flush:
+	$(CMD_SUDO) chmod +r $(CIRCLECI_BUILD_FOLDER) && $(CMD_RM) -R $(CIRCLECI_BUILD_FOLDER)
+
 help-circleci:
-	@echo "$(TEXT_FORMAT_BOLD)circleci$(TEXT_FORMAT_NORMAL)  		        		- Run CircleCI local build"
+	@echo "$(TEXT_FORMAT_BOLD)circleci-local-build$(TEXT_FORMAT_NORMAL)				- Run CircleCI local build"
+	@echo "$(TEXT_FORMAT_BOLD)circleci-flush$(TEXT_FORMAT_NORMAL)					- Remove CircleCI temporary folder"
--- misc/Docker.mk
+++ misc/Docker.mk
@@ -1,21 +1,21 @@
-DOCKER_BUILD_ARGS ?=
+DOCKER_BUILD_PARAMS ?=
 DOCKER_TAG = latest
 
 DOCKER_NAME = kleiberd/budapest-car-sharing-backend-$1
-DOCKER_BUILD_FN = $(CMD_DOCKER_BUILD) $(DOCKER_BUILD_ARGS) --build-arg SUBDIR=$1 -t $(DOCKER_NAME):$(DOCKER_TAG) .
-DOCKER_RUN_FN = $(CMD_DOCKER_RUN) $(CMD_DOCKER_RUN_ARGS) $(DOCKER_NAME):$(DOCKER_TAG) $2
+DOCKER_BUILD_FN = $(CMD_DOCKER_BUILD) $(DOCKER_BUILD_PARAMS) --build-arg SUBDIR=$1 -t $(DOCKER_NAME):$(DOCKER_TAG) .
+DOCKER_RUN_FN = $(CMD_DOCKER_RUN) $(DOCKER_RUN_PARAMS) $(DOCKER_NAME):$(DOCKER_TAG) $2
 
 docker-build-api:
 	$(call DOCKER_BUILD_FN,api)
 
-docker-build-builder-api: DOCKER_BUILD_ARGS=--target=builder
+docker-build-builder-api: DOCKER_BUILD_PARAMS=--target=builder
 docker-build-builder-api:
 	$(call DOCKER_BUILD_FN,api)
 
 docker-build-collector:
 	$(call DOCKER_BUILD_FN,collector)
 
-docker-build-builder-collector: DOCKER_BUILD_ARGS=--target=builder
+docker-build-builder-collector: DOCKER_BUILD_PARAMS=--target=builder
 docker-build-builder-collector:
 	$(call DOCKER_BUILD_FN,collector)
 
@@ -25,12 +25,15 @@ docker-run-tests-api:
 docker-run-tests-collector:
 	$(call DOCKER_RUN_FN,collector,go test)
 
-docker-run-api: CMD_DOCKER_RUN_ARGS=-it -p 8080:8080
+docker-run-api: DOCKER_RUN_PARAMS=-it -p 8080:8080
 docker-run-api:
 	$(call DOCKER_RUN_FN,api,/go/bin/api)
 
 docker-run-collector:
 	$(call DOCKER_RUN_FN,collector,/go/bin/collector)
+
+docker-copy-artifact-api:
+	$(call DOCKER_RUN_FN,api,/bin/sh -c "$(CMD_MKDIR) artifacts && $(CMD_CP) /go/bin/api /artifacts/api")
 
 help-docker:
 	@echo "$(TEXT_FORMAT_BOLD)docker-build-api$(TEXT_FORMAT_NORMAL)				- Build full API container"
